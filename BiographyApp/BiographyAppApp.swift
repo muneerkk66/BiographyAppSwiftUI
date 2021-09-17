af09df3b44c933e5ea2de072b91ae8c097ba2a98
Muneer KK <k.muneer@tcs.com>
2021-09-18 02:01:28 +0530

    Initial Commit

---
+++ BiographyApp/BiographyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiographyAppApp.swift
+//  BiographyApp
+//
+//  Created by Muneer KK on 18/09/21.
+//
+
+import SwiftUI
+
+@main
+struct BiographyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
